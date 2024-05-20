24b7a370b0391bd88022c0051c0a4bd07abdf272
ckroeger <ckroeger.hh@gmail.com>
2024-05-20 12:28:46 +0200

    reduce logo-size

--- newman/index.js
+++ newman/index.js
@@ -38,6 +38,7 @@ app.get('/collections', (req, res) => {
 
 app.get('/run/:collectionName', (req, res) => {
     const collectionName = req.params.collectionName;
+    const asJson = req.query.asJson === 'true'; // Check if asJson parameter is present and set to true
     if (!collectionName) {
         res.status(400).send('Missing collection name');
         return;
@@ -48,10 +49,66 @@ app.get('/run/:collectionName', (req, res) => {
             console.error(`File not found: ${collectionName}`);
             res.status(404).send(`Collection ${collectionName} not found`);
         } else {
-            runNewmanWith(res, location);
+            if (asJson) {
+                runNewmanWithJson(res, location, 'cli,json');
+            } else {
+                runNewmanWith(res, location, 'cli');
+            }
         }
     });
 });
+
+function runNewmanWithJson(res, location, reporters) {
+    if(reporters === 'cli') {
+        res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    }
+    else {
+        res.setHeader('Content-Type', 'application/json; charset=utf-8');
+    }
+    const nm = spawn('newman', [
+        'run', location, 
+        '--reporters', reporters,
+        '--reporter-json-export', '/home/newman/jsonReport.json'
+    ], { encoding: 'utf8' });
+    
+    nm.stdout.on('data', (data) => {
+        if(reporters === 'cli') {
+            res.write(data);
+        }
+    });
+    
+    nm.stdout.on('end', () => {
+        nm.on('close', (code) => {
+            //console.log(`child process exited with code ${code}`);
+            if(reporters === 'cli') {
+                if (code === 0) {
+                    res.write('\n✅ Success: Collection run completed successfully!\n');
+                } else {
+                    res.write(`\n❌ Fail: Collection run failed.\n`);
+                }
+            }
+            else {
+                fs.readFile('jsonReport.json', 'utf8', (err, data) => {
+                    if (err) {
+                        console.error(`Error reading jsonReport.json: ${err}`);
+                        res.status(500).send('Internal Server Error');
+                    } else {
+                        res.write(data);
+                        res.end();
+                    }
+                });
+            }
+        });
+    });
+    nm.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+        //res.write(`\nError: child process exited with code ${data}\n`);
+    });
+    nm.on('error', (err) => {
+        console.error(`spawn error: ${err}`);
+        res.status(500).send(`spawn Error: ${err}`);
+    });
+}
 
 app.get('/', (req, res) => {
     runNewmanWith(res, `${colPath}/mock-data-check.json`);
