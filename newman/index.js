89ee7349633a483e524489996e8b9623607d1745
Christian Kröger <c.kroeger@reply.de>
2024-07-10 09:59:31 +0200

    minor

--- newman/index.js
+++ newman/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const cors = require('cors');
 const app = express();
 const port = 3000;
-//const colPath = '/home/newman/collections';
+
 const rootPath = './';
 const colPath = rootPath + 'collections';
 
